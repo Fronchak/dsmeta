f0940c5904127d4f0eeddd7fc9a5b32601580bc0
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-11-16 23:01:52 -0300

    First component created

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/img/Vector.svg';
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="person-icon-container">
+            <img src={icon} />
+        </div>
+    )
+}
+
+export default NotificationButton;
