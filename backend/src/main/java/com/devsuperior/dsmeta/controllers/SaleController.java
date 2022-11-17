1f5508f8aa8b9fd9f35ac539b53fd646ac130580
Gabriel Fronchak Gmack <gabrielgmack@gmail.com>
2022-11-17 20:43:07 -0300

    Implement customized query to find sales between two dates

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/SaleController.java
@@ -3,6 +3,8 @@ package com.devsuperior.dsmeta.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devsuperior.dsmeta.entities.Sale;
@@ -22,7 +25,6 @@ public class SaleController {
 	@Autowired
 	private SaleService service;
 	
-	@GetMapping
 	public List<Sale> findAll() {
 		return service.findAll();
 	}
@@ -45,5 +47,13 @@ public class SaleController {
 	@DeleteMapping(value = "/{id}")
 	public void deleteById(@PathVariable(name = "id") Long id) {
 		service.deleteById(id);
+	}
+	
+	@GetMapping
+	public Page<Sale> findAllByDates(
+			@RequestParam(value="minDate", defaultValue = "") String minDate,
+			@RequestParam(value="maxDate", defaultValue = "") String maxDate,
+			Pageable pageable) {
+		return service.findAllByDates(minDate, maxDate, pageable);
 	}
 }
